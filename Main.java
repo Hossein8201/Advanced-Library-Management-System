a42481a88378c748e8647a229f4773f6ddb5ba72
hossein tatar <161024752+Hossein8201@users.noreply.github.com>
2024-10-21 00:08:28 +0330

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+/**
+ * <h1>This is the Main body of Library Management System</h1>
+ * In this class we handle and order the changes in the hole of Program and this is the only class that
+ * we can give it the values from the outside.
+ * <p>
+ * <b>Note:</b> We don't add this property to give from outside in this version.
+ *
+ * @author Hossein Tatar - 40133014
+ * @version 1.0
+ * @since 2021-10-21
+ */
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
